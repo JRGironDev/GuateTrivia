fe4a759800320256d070af153d7841da76e1be1b
Jose Quiñónez <joserobertoq@outlook.com>
2023-08-30 14:30:36 -0600

    Se modificó el card de inicio de sesión

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -2,7 +2,7 @@
     <!-- Session Status -->
     <x-auth-session-status class="mb-4" :status="session('status')" />
 
-    <form method="POST" action="{{ route('login') }}">
+    <form method="POST" action="{{ route('login') }}" novalidate>
         @csrf
 
         <!-- Email Address -->
@@ -26,22 +26,30 @@
 
         <!-- Remember Me -->
         <div class="block mt-4">
-            <label for="remember_me" class="inline-flex items-center">
+            <label for="Recordarme" class="inline-flex items-center">
                 <input id="remember_me" type="checkbox" class="rounded dark:bg-gray-900 border-gray-300 dark:border-gray-700 text-indigo-600 shadow-sm focus:ring-indigo-500 dark:focus:ring-indigo-600 dark:focus:ring-offset-gray-800" name="remember">
                 <span class="ml-2 text-sm text-gray-600 dark:text-gray-400">{{ __('Remember me') }}</span>
             </label>
         </div>
 
-        <div class="flex items-center justify-end mt-4">
+        <div class="flex items-center justify-between mt-4 mb-7">
             @if (Route::has('password.request'))
-                <a class="underline text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800" href="{{ route('password.request') }}">
-                    {{ __('Forgot your password?') }}
-                </a>
-            @endif
+                <x-link
+                    :href="route('register')"
+                >
+                    Crear cuenta
+                </x-link>
 
-            <x-primary-button class="ml-3">
-                {{ __('Log in') }}
-            </x-primary-button>
+                <x-link
+                    :href="route('password.request')"
+                >
+                    Olvidaste tu contraseña?
+                </x-link>
+            @endif
         </div>
+
+        <x-primary-button class="w-full justify-center">
+            {{ __('Iniciar Sesión') }}
+        </x-primary-button>
     </form>
 </x-guest-layout>
--- resources/views/components/input-error.blade.php
+++ resources/views/components/input-error.blade.php
@@ -3,7 +3,7 @@
 @if ($messages)
     <ul {{ $attributes->merge(['class' => 'text-sm text-red-600 dark:text-red-400 space-y-1']) }}>
         @foreach ((array) $messages as $message)
-            <li>{{ $message }}</li>
+            <li class="bg-red-100 p-2 border-l-4 border-red-600 text-red-600 font-bold">{{ $message }}</li>
         @endforeach
     </ul>
 @endif
