ad2caf670bf75963d92cd3ca431bfc7e5c3de940
Jose Quiñónez <joserobertoq@outlook.com>
2023-10-09 22:21:47 -0600

    Fix aboutME and birthat in register

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -40,6 +40,8 @@ class RegisteredUserController extends Controller
             'name' => $request->name,
             'email' => $request->email,
             'password' => Hash::make($request->password),
+            'aboutMe' => "",
+            'birthday' => $request->nully
         ]);
 
         event(new Registered($user));
