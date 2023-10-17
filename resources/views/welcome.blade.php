15e311ac2a8dbaf08bc5a1575ba992e9faf4ca9f
22006116 <22006116@galileo.edu>
2023-10-17 13:06:10 -0600

    cambios en los titulos

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,7 +8,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="{{URL::asset('css/styles.css')}}">
     @vite(['resources/css/app.css', 'resources/js/app.js'])
-    <title>Home</title>         
+    <title>{{ config('app.name', 'GuateTrivia') }}</title>         
 </head>
 <body>
     <header>
