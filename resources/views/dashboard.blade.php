cb139cbac4b0dd70e3354cc337530e6f274d5340
22006116 <22006116@galileo.edu>
2023-10-18 15:39:08 -0600

    Cambio de apariencia en dashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,17 +1,18 @@
-<x-app-layout>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
-            {{ __('Dashboard') }}
-        </h2>
-    </x-slot>
+@extends('layouts.page')
 
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900 dark:text-gray-100">
-                    {{ __("You're logged in!") }}
-                </div>
+@section('content')
+    <div class="container-fluid container-full-height">
+        <div class="row">
+        <div class="col comic-style">
+            <div class="avatar-container">
+            <img src="images/ninio3.jpg" alt="Avatar" class="avatar">
             </div>
         </div>
+        <div class="col comic-style">
+            <div id="play-button" class="icons">
+            <a href="/" class="mr-2"><i class="fa-solid fa-circle-play fa-10x"></i></i></a>      
+            </div>
+        </div>
+       
     </div>
-</x-app-layout>
+@endsection
---
+++ resources/views/layouts/page.blade.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="UTF-8"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css"/>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.0/css/all.min.css"/>
+    @vite(['resources/css/styles.css'])
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+    
+    <title>Página con estilo de cómic</title>   
+  
+</head>
+<body>
+ <!-- Page Heading -->   
+
+  <div class="comic-header" style="background: url('images/bus.png');">
+    <img src="images/jmarroquin.jpg" alt="Imagen circular" class="img-circle">
+    <div class="content">
+      <h2>{{ Auth::user()->name }}</h2>
+      <div class="progress">
+        <div class="progress-bar w-75" role="progressbar" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100"></div>
+      </div>
+      <h2>955 Pts.</h2>
+    </div>
+    <div class="icons">
+      <a href="/" class="mr-2"><i class="fas fa-medal fa-3x"></i></a>
+      <a href="{{ __('profile') }}"><i class="fas fa-gear fa-3x"></i></a>
+    </div>
+  </div>
+
+  <!-- Page Heading --> 
+
+ <!-- Page Content -->
+ <main>
+  @yield('content')
+ </main> 
+ <!-- Page Content -->
+
+  <script src="https://kit.fontawesome.com/a076d05399.js" ></script>
+  <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
