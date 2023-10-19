7ffc69d78a5fe189614d4dff8365607bd1aacbf2
22006116 <22006116@galileo.edu>
2023-10-19 07:46:28 -0600

    Se coloca ícono de agregar amigos en dashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -5,12 +5,15 @@
         <div class="row">
         <div class="col comic-style">
             <div class="avatar-container">
-            <img src="images/ninio3.jpg" alt="Avatar" class="avatar">
+                <img src="images/ninio3.jpg" alt="Avatar" class="avatar">
             </div>
         </div>
         <div class="col comic-style">
             <div id="play-button" class="icons">
-            <a href="/" class="mr-2"><i class="fa-solid fa-circle-play fa-10x"></i></i></a>      
+                <a href="/" class="mr-2"><i class="fa-solid fa-circle-play fa-10x"></i></i></a>      
+            </div>
+            <div class="add-friends">
+                <a href="/" class="mr-2"><i class="fas fa-user-plus fa-3x"></i></a>
             </div>
         </div>
        
